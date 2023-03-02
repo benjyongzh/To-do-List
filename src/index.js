4b3b9b9a4e66a3d7f04ac4143ac47f9063e4193d
Benjamin Yong <benj_yongzh@hotmail.com>
2023-03-03 00:12:21 +0800

    Start firebase.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import * as projectManager from "./components/projectManager";
 import * as DOMManager from "./components/DOMManager";
 import './style.css';
-//import taskComponent from "./components/task";
-//import * as eventManager from "./components/eventManager";
 
+//firebase
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/database';
 
+const firebaseConfig = {
+    apiKey: "AIzaSyA0xvhI-PUNhXci8ascCup-QzpNeTw_u88",
+  authDomain: "to-do-list-98ea9.firebaseapp.com",
+  projectId: "to-do-list-98ea9",
+  storageBucket: "to-do-list-98ea9.appspot.com",
+  messagingSenderId: "861087320606",
+  appId: "1:861087320606:web:a112509860e4b5f6793c0f"
+};
 
-
-/* const newProject = projectComponent("myprojectname", "somedesc");
-const someTask = taskComponent("some task", "i gotta do this", "medium");
-newProject.addTask(someTask);
-console.log(newProject); */
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
